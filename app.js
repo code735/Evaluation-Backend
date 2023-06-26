574488be3b88310c1ae0b8bf11dc380be3eb414b
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-06-26 22:36:34 +0530

    cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
@@ -197,7 +200,7 @@ app.get('/todos/:todoId', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
